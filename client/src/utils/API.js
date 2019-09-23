f1758820c8ae9d7fcd553ce6a5beba0ebe48e39d
Ankit Prakash <ankit.prakash687@gmail.com>
2019-09-22 23:43:45 -0500

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/codeword/api/v1/",
+  baseURL: "http://localhost:5000/codeword/api/v1/",
   responseType: "json"
 });
