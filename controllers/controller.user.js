767e40d751c6778d132151017987779b06738780
Ankit Prakash <ankit.prakash687@gmail.com>
2019-10-16 17:45:18 -0500

    Update reset password html

--- controllers/controller.user.js
+++ controllers/controller.user.js
@@ -55,26 +55,48 @@ var signUp = (req,res) => {
             userModel.save().then((user) => {
                 if(user){
 
-                    const mailoptions = {
-                        from: 'codeword.group03@gmail.com', 
-                        to: body.email, 
-                        subject: "Link To Reset Password", 
-                        text:'Hi, \n\n'+
-                            'You are receiving this because you(or someone else) have requested the reset'+ 
-                            'of the password for your account.\n\nPlease click on the following link,'+
-                            'or paste this into your browser to complete the process within one hour of' +
-                            ' receiving it:\n\n' + 'https://codeword-group03.herokuapp.com/verifyEmail/'+token+'\n\n' + 
-                            'If you did not request this, please ignore this email and your password will remain unchanged.\n\n'+
-                            'Thank you!\n'+
-                            'Team codeword group03'
-                    }
-                console.log('sending mail')
-    
-                        transporter.sendMail(mailoptions, function (err, response) {
-                            if (err) {
-                                res.json({code: 400, message:err});
-                            }
-                    })
+                    readHTMLFile(__dirname + '/resetPassword.html', function(err, html) {
+
+                        console.log( __dirname+'/images/forget_password.png')
+                        var template = handlebars.compile(html);
+                        var replacements = {
+                            username: user.first_name + ' ' + user.last_name,
+                            image1: '/images/forget_password.png',
+                            url:'https://codeword-group03.herokuapp.com/verifyEmail/'+token,
+                            type: 'Verify Email',
+                            message1: 'There\'s just one more step before you get to the fun part.',
+                            message2: 'Verify we have the right email address by clicking on the button below:'
+                       };
+                    const transporter = nodemailer.createTransport({
+                        service: 'gmail', 
+                        auth: {
+                                user: 'codeword.group03@gmail.com',
+                                pass: 'Aug@2019'
+                             }
+                    });
+
+                const mailoptions = {
+                    from: 'codeword.group03@gmail.com', 
+                    to: body.email, 
+                    subject: "Link To Reset Password", 
+                    text:'Hi, \n\n'+
+                        'You are receiving this because you(or someone else) have requested the reset'+ 
+                        'of the password for your account.\n\nPlease click on the following link,'+
+                        'or paste this into your browser to complete the process within one hour of' +
+                        ' receiving it:\n\n' + 'https://codeword-group03.herokuapp.com/resetPassword/'+token+'\n\n' + 
+                        'If you did not request this, please ignore this email and your password will remain unchanged.\n\n'+
+                        'Thank you!\n'+
+                        'Team codeword group03',
+                    html: template(replacements)
+                }
+            console.log('sending mail')
+
+                    transporter.sendMail(mailoptions, function (err, response) {
+                        if (err) {
+                            res.json({code: 400, message:err});
+                        } 
+                })
+            })
 
                     var newToken = jwt.sign({email: body.email, id: user.id },'codewordnwmsu',{expiresIn:  10000 * 3000 }).toString();
                     console.log(newToken)
@@ -526,10 +548,14 @@ var requests = (req,res) =>{
                         res.json({code: 400, message:'Something went wrong'});
                     }
                     readHTMLFile(__dirname + '/resetPassword.html', function(err, html) {
+
+                        console.log( __dirname+'/images/forget_password.png')
                         var template = handlebars.compile(html);
                         var replacements = {
                             username: user.first_name + ' ' + user.last_name,
+                            image1: '/images/forget_password.png',
                             url:'https://codeword-group03.herokuapp.com/resetPassword/'+token,
+                            type: 'Reset Password',
                             message1: 'There was a request to change your password!',
                             message2: 'If did not make this request, just ignore this email. Otherwise, please click the button below to change your password:'
                        };
