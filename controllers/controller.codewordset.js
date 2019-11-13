cf65c01eb0fcf2db0de437cefc07a0035f6ce39f
Ankit Prakash <ankit.prakash687@gmail.com>
2019-11-13 09:22:33 -0600

    Update generate report controller

--- controllers/controller.codewordset.js
+++ controllers/controller.codewordset.js
@@ -339,7 +339,7 @@ let addcodeword = (req, res) => {
       // console.log(Array.from(new Set(final.map(JSON.stringify)), JSON.parse))
      
      var anagrams = anagramFinder.find(codewords).filter((item)=>{
-         if(item.length > 1){
+         if(item.length > 0){
              return item
          }
      })
